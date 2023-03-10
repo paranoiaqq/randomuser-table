3885cd6d48f35e4b6b598dd703190d990968093f
paranoiaqq <nikitabelousov@ymail.com>
2023-03-11 01:07:26 +0300

    feat: added user model

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,3 +1,42 @@
 export default interface IUser {
   gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  dob: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
 }
