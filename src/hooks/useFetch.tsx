42d35d90bc50223c3aadf31bd9fd296cd632f2f1
paranoiaqq <nikitabelousov@ymail.com>
2023-03-11 01:07:50 +0300

    feat: added useFetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,40 @@
+import IUser from "../models/IUser";
+import { useEffect, useState } from "react";
+
+export interface IResponse<T> {
+  status: Number;
+  statusText: String;
+  data: T;
+  error: unknown;
+  loading: Boolean;
+}
+
+const useFetch = <T,>(url: string): IResponse<T> => {
+  const [status, setStatus] = useState<Number>(0);
+  const [statusText, setStatusText] = useState<String>("");
+  const [data, setData] = useState<T>();
+  const [error, setError] = useState<unknown>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const apiResponse = await fetch(url);
+      const json = await apiResponse.json();
+      setStatus(apiResponse.status);
+      setStatusText(apiResponse.statusText);
+      setData(json);
+    } catch (error) {
+      setError(error as string);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { status, statusText, data, error, loading } as IResponse<T>;
+};
+
+export default useFetch;
